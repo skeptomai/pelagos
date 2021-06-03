fce6e7d6d1a2b9a3679d2c022d9a81ef9ddd5b87
Christopher Brown <cb@skeptomai.com>
2021-06-03 08:13:39 -0700

    trim to single clone

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,6 @@ use std::ptr;
 
 use unshare::{Child, Command, Error, Stdio};
 
-/// Not really 'fork_exec' but plays the role here
-fn fork_exec() -> Result<Child, Error> {
-    let self_exe = palaver::env::exe_path();
-    let new_args: Vec<_> = std::env::args_os().skip(1).collect();
-    Command::new(self_exe.unwrap())
-        .args(&new_args)
-        .arg0("child")
-        .unshare([unshare::Namespace::Uts, unshare::Namespace::Pid].iter())
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .spawn()
-}
-
 /// callback that mounts a new proc filesystem
 /// this cannot allocate
 fn mount_proc() -> std::io::Result<()> {
@@ -41,7 +28,14 @@ fn mount_proc() -> std::io::Result<()> {
 fn child() -> Result<Child, Error> {
     unsafe {
         Command::new(args().nth(1).unwrap())
-            .unshare([unshare::Namespace::Uts, unshare::Namespace::Pid].iter())
+            .unshare(
+                [
+                    unshare::Namespace::Uts,
+                    unshare::Namespace::Pid,
+                    unshare::Namespace::User,
+                ]
+                .iter(),
+            )
             .stdin(Stdio::inherit())
             .stdout(Stdio::inherit())
             .chroot_dir("/home/rootfs")
@@ -57,21 +51,7 @@ fn main() {
         panic!("Not enough argument supplied.  Gotta run something!")
     }
 
-    // first launch is normal exe with process name
-    // second launch is a spawn of same exe with 'child' as argv[0]
-    match args().nth(0).as_deref() {
-        Some("child") => {
-            println!("CHILD: {}", std::process::id());
-            panic_spawn(&child);
-        }
-        Some(_) => {
-            println!("PARENT: {}", std::process::id());
-            panic_spawn(&fork_exec);
-        }
-        _ => {
-            panic!("NEITHER PARENT NOR CHILD?");
-        }
-    }
+    panic_spawn(&child);
 }
 
 fn panic_spawn(p: &(dyn Fn() -> Result<Child, Error>)) {
